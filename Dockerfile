228800fe59d421585fdee76c2665e04fec92fc50
10151032 <s-hyun.lee@kt.com>
2022-05-03 14:56:53 +0900

    simplejwt logout 기능 구현(api/logout)

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM sjoh0704/django:latest
+RUN pip3 install django
+WORKDIR /usr/src/app
+COPY . .
+WORKDIR ./project
+CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+EXPOSE 8000
--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('post/list/', views.ApiPostLV.as_view(), name='post_list'),
     path('logout/', views.ApiLogoutView.as_view(), name='auth_logout'),
 
+
     # path('login/', views.ApiLoginView.as_view(), name='login')
 ]
